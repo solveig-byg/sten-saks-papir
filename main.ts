58aba6d7b4ed07521f692efc542bf454410205fa
solveig-byg <83781463+solveig-byg@users.noreply.github.com>
2021-05-06 20:23:21 +0200

    De første filer til MakeCode-projekt

---
+++ main.ts
@@ -0,0 +1,29 @@
+let terning = 0
+input.onGesture(Gesture.Shake, function () {
+    terning = randint(1, 3)
+    if (terning == 1) {
+        basic.showLeds(`
+            . # # # .
+            # # # # #
+            # # # # #
+            # # # # #
+            . # # # .
+            `)
+    } else if (terning == 2) {
+        basic.showLeds(`
+            # # . . #
+            # # . # .
+            . . # . .
+            # # . # .
+            # # . . #
+            `)
+    } else if (terning == 3) {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    }
+})
